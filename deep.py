4b9da0dc2e9cb16335fa7976d7159351cff0bf0b
LHSaoMai <151466677+LHSaoMai@users.noreply.github.com>
2024-09-21 23:55:17 +0200

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,5 @@
+say = input('What is the Answer to the Great Question of Life, the Universe, and Everything? ').lower().strip()
+if say=='42' or say=='forty-two' or say=='forty two':
+    print('Yes')
+else:
+    print('No')
